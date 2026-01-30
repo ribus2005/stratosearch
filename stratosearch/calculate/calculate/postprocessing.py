476d79c1597d0b4f7fddf6c855452d9d33fec2bb
buklinfur <buklinfur@outlook.com>
2026-01-30 19:48:23 +0700

    DPT processing

--- stratosearch/calculate/calculate/postprocessing.py
+++ stratosearch/calculate/calculate/postprocessing.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import cv2
 
 
 def default_postprocess(output: torch.Tensor) -> np.ndarray:
@@ -13,5 +14,16 @@ def default_postprocess(output: torch.Tensor) -> np.ndarray:
     return class_map.astype(np.uint8)
 
 
+def DPT_postprocess(output: torch.Tensor, shape = (701, 255)) -> np.ndarray:
+    pred = output.predicted_depth.argmax(dim=1).squeeze()
+
+    pred_np = pred.detach().cpu().numpy() 
+
+    resized = cv2.resize(pred_np, shape, interpolation=cv2.INTER_NEAREST)
+
+    return resized
+
+
 POSTPROCESSORS = {
+    'DPT': DPT_postprocess
 }
--- stratosearch/calculate/calculate/preprocessing.py
+++ stratosearch/calculate/calculate/preprocessing.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import torchvision.transforms as transforms
 
 
 def default_preprocess(arr: np.ndarray) -> torch.Tensor:
@@ -10,6 +11,17 @@ def unet_preprocess(arr: np.ndarray) -> torch.Tensor:
     return torch.from_numpy(arr).float()[None, None, ...]
 
 
+def DPT_preprocess(img: np.ndarray) -> torch.Tensor:
+    img_transform = transforms.Compose([
+                                transforms.ToTensor(),
+                                transforms.Lambda(lambda x: x.repeat(3, 1, 1))
+                                ])
+    input = img_transform(img)
+
+    return input.unsqueeze(0)
+
+
 PREPROCESSORS = {
     'unet': unet_preprocess,
+    'DPT': DPT_preprocess
 }
